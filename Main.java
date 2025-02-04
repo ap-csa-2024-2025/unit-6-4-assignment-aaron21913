e870afce30b58a7998472c8cb6a79645fbeb8be4
aaron cheese skibidi rizz <126525235+aaron21913@users.noreply.github.com>
2025-02-04 18:12:08 +0000

    rizz

--- Main.java
+++ Main.java
@@ -10,25 +10,75 @@ public class Main
 
   public static int countLength(String[] arr, int targetLength)
   {
-    // replace with your code
-    return 0;
-  }
+	  int count = 0;
+
+	  for (int i = 0; i < arr.length; i++)
+	  {
+		  if (arr[i].length() == (targetLength))
+	  	{
+	  		count++;
+		  }
+	  }
+  return count;
+}
+
 
   public static int indexOf(double[] arr, double target)
   {
-    // replace with your code
-    return -1;
-  }
+	  int count = 0;
+
+  	for (int i = 0; i < arr.length; i++)
+	  {
+  		if (arr[i].length() == (targetLength)
+		  {
+		  	count++;
+		  }
+	  }
+  return count;
+}
+
 
   public static boolean hasDuplicates(int[] arr)
   {
-    // replace with your code
+    for (int i = 0; i > arr.length; i++)
+    {
+      int target = arr.[i];
+      
+      for (int j = i + 1; j < arr.length; j++)
+      {
+        if(arr[j] == target)
+        {
+          return true;
+        }
+      }
+    }
     return false;
   }
 
   public static String findMode(String[] arr)
   {
-    // replace with your code
-    return null;
+    int count = 0;
+    int mode = arr[0];
+    int max_count = 0;
+    for (int i = 0; i < arr.length; i++)
+    {
+      int target = arr.[1];
+      count = 0;
+
+      for (int j = 0; j < arr.length; j++)
+      {
+        if(arr[j].equals(target))
+        {
+          count++
+        }
+      }
+      
+      if(count > max_count)
+      {
+        max_count = count;
+        mode = arr[i];
+      }
+    }
+    return mode;
   }
 }
